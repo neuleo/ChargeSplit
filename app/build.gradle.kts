147bf979c6ee037b7786232b749accc688532d37
Leon <leon.neu27@gmail.com>
2026-06-18 12:34:23 +0200

    chore(release): Bump version to 1.1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.neuleo.chargesplit"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.1.5"
+        versionCode = 8
+        versionName = "1.1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
